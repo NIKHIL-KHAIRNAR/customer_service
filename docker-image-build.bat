4d586ce358ac8f342122b17f86e7edacf845ac4f
Nikhil <nikhkhairnar13@gmail.com>
2019-11-30 17:47:24 +0530

    "final successful commit"

--- docker-image-build.bat
+++ docker-image-build.bat
@@ -1,4 +1,5 @@
 SET PATH=%PATH%;C:\Users\Nikhil\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
+SET BUILD_NUMBER=12
 SET IMAGE_NAME=gcr.io/customer-v1/customer-image:%BUILD_NUMBER%
-cd C:\Users\Nikhil\Desktop\Microservices
+cd C:\Users\Nikhil\Desktop\customer_service
 gcloud builds submit --tag %IMAGE_NAME% .
--- kubernetes-deploy.bat
+++ kubernetes-deploy.bat
@@ -1,5 +1,5 @@
 SET PATH=%PATH%;C:\Users\Nikhil\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
-SET BUILD_NUMBER=53
-SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
-cd C:\Users\Nikhil\Desktop\Microservices
-kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
+SET BUILD_NUMBER=12
+SET IMAGE_NAME=gcr.io/customer-v1/customer-image:%BUILD_NUMBER%
+cd C:\Users\Nikhil\Desktop\customer_service
+kubectl set image deployment/py-customer py-customer=%IMAGE_NAME%
